b5dce91f59975283cb2745e42268a6fbce27ac75
SebatianDte <sebateumate@outlook.com>
2025-07-25 09:17:39 -0300

    Primera subida

---
+++ dev-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+    return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-5xl font-extrabold">¡Hola Sebi! Tailwind está funcionando.</h1>
+    </div>
+  )
+}
+
+export default App
